c576d817424fe7222855a85528cbc18567b6ed22
johankivi <johan@kivi.nu>
2019-11-22 08:43:57 +0100

    added lightbox

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.gallery img').forEach(img => {
+    img.addEventListener('click', (e) => {
+        lightbox(e.target);
+    })
+})
+
+
+function lightbox(img){
+    // get info
+    let imgUrl = img.attributes.src.value;
+    let title = img.attributes.alt.value;
+
+    // Set content elements
+    document.querySelector('#overlay img').setAttribute('src', imgUrl);
+    document.querySelector('#overlay h1').innerHTML = title;
+    document.querySelector('#overlay a').setAttribute('href', imgUrl);
+
+    // Toggle lightbox
+    document.querySelector('#overlay').classList.toggle('show');
+}
+
+// Close overlay
+document.querySelector('#overlay article').addEventListener('click', () => {
+    document.querySelector('#overlay').classList.toggle('show')
+})
